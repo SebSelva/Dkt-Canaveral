7cb75e1cae6a95c792d31fa378a4bca1ee010e96
Sébastien Selvanayagom <sebastien.selvanayagom.partner@decathlon.com>
2021-06-24 13:54:39 +0200

    DARTS-15 - Navigation changes

--- app/src/main/java/com/decathlon/canaveral/common/BaseActivity.kt
+++ app/src/main/java/com/decathlon/canaveral/common/BaseActivity.kt
@@ -13,24 +13,6 @@ abstract class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(getLayoutId())
-
-        //Initialize Bottom Navigation View.
-        val navView = findViewById<BottomNavigationView>(R.id.bottomNav_view)
-
-        //Pass the ID's of Different destinations
-
-        //Pass the ID's of Different destinations
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_dashboard,
-            R.id.navigation_stats,
-            R.id.navigation_settings
-        ).build()
-
-        //Initialize NavController
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        NavigationUI.setupWithNavController(navView, navController)
     }
 
     abstract fun getLayoutId() : Int
---
+++ app/src/main/java/com/decathlon/canaveral/game/GameActivity.kt
@@ -0,0 +1,11 @@
+package com.decathlon.canaveral.game
+
+import com.decathlon.canaveral.R
+import com.decathlon.canaveral.common.BaseActivity
+
+class GameActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_game
+    }
+}
