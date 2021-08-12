350c5169bcead0cb921930a35a0c6c22d92540f6
Sebastien Selvanayagom <sebastien.selvanayagom.partner@decathlon.com>
2021-08-12 13:15:47 +0200

    DARTS-43 - CountUp Play

---
+++ app/src/main/java/com/decathlon/canaveral/game/countup/CountUpViewModel.kt
@@ -0,0 +1,22 @@
+package com.decathlon.canaveral.game.countup
+
+import com.decathlon.canaveral.Interactors
+import com.decathlon.canaveral.common.model.PlayerPoint
+import com.decathlon.canaveral.common.model.Point
+import com.decathlon.canaveral.common.utils.DartsUtils
+import com.decathlon.canaveral.game.x01.Game01ViewModel
+
+class CountUpViewModel(interactors: Interactors) : Game01ViewModel(interactors) {
+
+    override fun addPlayerPoint(point: Point) {
+        isStackIncreasing = true
+        isRoundDecreasing = false
+        currentPlayer?.let {
+            if (DartsUtils.getPlayerRoundDarts(it, currentRound, playersPoints).size < DartsUtils.DARTS_SHOTS_NUMBER) {
+                currentRound = DartsUtils.getRoundNumber(players, playersPoints)
+                playersPoints.push(PlayerPoint(it, point, currentRound, false))
+                playersPointsLivedata.postValue(playersPoints)
+            }
+        }
+    }
+}
